ed4b2d1566e072821f951566857cdabbb4937b78
Alessandro <alessandro.caselli446@gmail.com>
2023-12-14 12:51:57 +0100

    sistemazioni

--- invkinDaVinci.m
+++ invkinDaVinci.m
@@ -10,19 +10,19 @@ T40 = T10*T21*T32*T43;
 TRCMw = p.T0w * T40 * transX(p.alphai(5)+90) * transP(0,p.di(5)+p.ai(8),0);
 % posizione del target rispetto alla terna RCM
 A = TRCMw \ P;
-x = A(1);
-y = A(2);
-z = A(3);
+x = A(1)
+y = A(2)
+z = A(3)
 % parametri DH
 a6 = p.ai(7);
 % a7 = p.ai(8);
 a8 = p.ai(9);
 
 % inclinazione del piano di lavoro del braccio rispetto a al piano verticale z4y4
-q5 = atan2(-z,x) + 270;
+q5 = atan2(-z,x) + 200;
 q6 = atan2(y,z/sind(q5)) + asind(a8/a6);
 q7 = 3 * 180 / 2 - q6;
 q8 = atan2(y,z/sind(q5));
-q9 = sqrt(a6^2 - a8^2) + sqrt(x^2 + y^2 + z^2) - p.Ltool;
+q9 = sqrt(a6^2 - a8^2) + sqrt(x^2 + y^2 + z^2) - p.Ltool %- 1200;
 % soluzione (punto robot)
 q = [q5 q6 q7 q8 q9];
